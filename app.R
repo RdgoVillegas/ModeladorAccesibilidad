9486f9f3a0b6fb5c40ff79b11d4c17e4068fcaa0
unknown <rdgo.villegas@gmail.com>
2021-09-28 12:51:32 -0300

    añadida pseudo metadata a app.R

--- app.R
+++ app.R
@@ -1,11 +1,7 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+#### Shiny App code ####
+# Author: Rodrigo Villegas Salgado
+# Email: rdvillegas@uc.cl
+# Status: Development
 
 
 # Neceisto implementar un botón que diga "añadir colegio" que haga lo siguiente:
